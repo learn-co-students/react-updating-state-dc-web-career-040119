9db91c3ad0e619255bb2a464aa5e308d5294f836
github username <github email address>
2019-06-25 11:55:26 -0400

    my code

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,26 @@
-// Code ClickityClick Component Here
+import React from 'react'
+
+class ClickityClack extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+      hasBeenClicked: false
+    }
+  }
+
+handleClick = () => {
+    this.setState({
+      hasBeenClicked: true
+    }),() => {console.log(this.state.hasBeenClicked);}
+  }
+
+  render(){
+    return(
+      <div>
+        <p>I have {this.state.hasBeenClicked ? null : 'not'}</p>
+        <button onClick={this.handleClick}>Click Me!</button>
+      </div>
+    )}
+}
+export default ClickityClack
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ClickityClack from './components/ClickityClick'
 
 ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
+  < ClickityClack />,
   document.getElementById('root')
 );
